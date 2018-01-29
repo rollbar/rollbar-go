6df0cd005485475ed05291dbdcc58e5332c58bea
Andrew Weiss <wvvwwvw@gmail.com>
2018-01-29 10:41:55 -0800

    finish refactoring the two clients into one with two different transports

--- client.go
+++ client.go
@@ -3,113 +3,312 @@ package rollbar
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
 	"regexp"
 	"runtime"
-	"time"
 )
 
-type Client interface {
+// AsyncClient is the default concrete implementation of the Client interface
+// which sends all data to Rollbar asynchronously.
+type Client struct {
 	io.Closer
-
-	// Rollbar access token.
-	SetToken(token string)
-	// All errors and messages will be submitted under this environment.
-	SetEnvironment(environment string)
-	// Set the Platform to be reported for all items
-	SetPlatform(platform string)
-	// String describing the running code version on the server
-	SetCodeVersion(codeVersion string)
-	// host: The server hostname. Will be indexed.
-	SetServerHost(serverHost string)
-	// root: Path to the application code root, not including the final slash.
-	// Used to collapse non-project code when displaying tracebacks.
-	SetServerRoot(serverRoot string)
-	// custom: Any arbitrary metadata you want to send.
-	SetCustom(custom map[string]interface{})
-	// Whether or not to use custom client-side fingerprint
-	SetFingerprint(fingerprint bool)
-
-	// Rollbar access token.
-	Token() string
-	// All errors and messages will be submitted under this environment.
-	Environment() string
-	// Platform is the platform reported for all Rollbar items. The default is
-	// the running operating system (darwin, freebsd, linux, etc.) but it can
-	// also be application specific (client, heroku, etc.).
-	Platform() string
-	// String describing the running code version on the server
-	CodeVersion() string
-	// host: The server hostname. Will be indexed.
-	ServerHost() string
-	// root: Path to the application code root, not including the final slash.
-	// Used to collapse non-project code when displaying tracebacks.
-	ServerRoot() string
-	// custom: Any arbitrary metadata you want to send.
-	Custom() map[string]interface{}
-	// Whether or not to use custom client-side fingerprint
-	Fingerprint() bool
-
-	// Error sends an error to Rollbar with the given severity level.
-	Error(level string, err error)
-	// Errorf sends an error to Rollbar with the given format string and arguments.
-	Errorf(level string, format string, args ...interface{})
-	// ErrorWithExtras sends an error to Rollbar with the given severity
-	// level with extra custom data.
-	ErrorWithExtras(level string, err error, extras map[string]interface{})
-	// ErrorWithStackSkip sends an error to Rollbar with the given severity
-	// level and a given number of stack trace frames skipped.
-	ErrorWithStackSkip(level string, err error, skip int)
-	// ErrorWithStackSkipWithExtras sends an error to Rollbar with the given
-	// severity level and a given number of stack trace frames skipped with
-	// extra custom data.
-	ErrorWithStackSkipWithExtras(level string, err error, skip int, extras map[string]interface{})
-
-	// RequestError sends an error to Rollbar with the given severity level
-	// and request-specific information.
-	RequestError(level string, r *http.Request, err error)
-	// RequestErrorWithExtras sends an error to Rollbar with the given
-	// severity level and request-specific information with extra custom data.
-	RequestErrorWithExtras(level string, r *http.Request, err error, extras map[string]interface{})
-	// RequestErrorWithStackSkip sends an error to Rollbar with the given
-	// severity level and a given number of stack trace frames skipped, in
-	// addition to extra request-specific information.
-	RequestErrorWithStackSkip(level string, r *http.Request, err error, skip int)
-	// RequestErrorWithStackSkipWithExtras sends an error to Rollbar with
-	// the given severity level and a given number of stack trace frames
-	// skipped, in addition to extra request-specific information and extra
-	// custom data.
-	RequestErrorWithStackSkipWithExtras(level string, r *http.Request, err error, skip int, extras map[string]interface{})
-
-	// Message sends a message to Rollbar with the given severity level.
-	Message(level string, msg string)
-	// MessageWithExtras sends a message to Rollbar with the given severity
-	// level with extra custom data.
-	MessageWithExtras(level string, msg string, extras map[string]interface{})
-
-	// RequestMessage sends a message to Rollbar with the given severity level
-	// and request-specific information.
-	RequestMessage(level string, r *http.Request, msg string)
-	// RequestMessageWithExtras sends a message to Rollbar with the given
-	// severity level and request-specific information with extra custom data.
-	RequestMessageWithExtras(level string, r *http.Request, msg string, extras map[string]interface{})
+	Transport     Transport
+	configuration configuration
 }
 
+// New returns the default implementation of a Client
+func New(token, environment, codeVersion, serverHost, serverRoot string) *Client {
+	return NewAsync(token, environment, codeVersion, serverHost, serverRoot)
+}
+
+// NewAsync builds an asynchronous implementation of the Client interface
+func NewAsync(token, environment, codeVersion, serverHost, serverRoot string) *Client {
+	configuration := createConfiguration(token, environment, codeVersion, serverHost, serverRoot)
+	transport := NewTransport(token, configuration.endpoint)
+	return &Client{
+		Transport:     transport,
+		configuration: configuration,
+	}
+}
+
+func NewSync(token, environment, codeVersion, serverHost, serverRoot string) *Client {
+	configuration := createConfiguration(token, environment, codeVersion, serverHost, serverRoot)
+	transport := NewSyncTransport(token, configuration.endpoint)
+	return &Client{
+		Transport:     transport,
+		configuration: configuration,
+	}
+}
+
+// Rollbar access token.
+func (c *Client) SetToken(token string) {
+	c.configuration.token = token
+	c.Transport.SetToken(token)
+}
+
+// All errors and messages will be submitted under this environment.
+func (c *Client) SetEnvironment(environment string) {
+	c.configuration.environment = environment
+}
+
+// The endpoint to post items to
+func (c *Client) SetEndpoint(endpoint string) {
+	c.configuration.endpoint = endpoint
+	c.Transport.SetEndpoint(endpoint)
+}
+
+// Set the Platform to be reported for all items
+func (c *Client) SetPlatform(platform string) {
+	c.configuration.platform = platform
+}
+
+// String describing the running code version on the server
+func (c *Client) SetCodeVersion(codeVersion string) {
+	c.configuration.codeVersion = codeVersion
+}
+
+// host: The server hostname. Will be indexed.
+func (c *Client) SetServerHost(serverHost string) {
+	c.configuration.serverHost = serverHost
+}
+
+// root: Path to the application code root, not including the final slash.
+// Used to collapse non-project code when displaying tracebacks.
+func (c *Client) SetServerRoot(serverRoot string) {
+	c.configuration.serverRoot = serverRoot
+}
+
+// custom: Any arbitrary metadata you want to send.
+func (c *Client) SetCustom(custom map[string]interface{}) {
+	c.configuration.custom = custom
+}
+
+// Whether or not to use custom client-side fingerprint
+func (c *Client) SetFingerprint(fingerprint bool) {
+	c.configuration.fingerprint = fingerprint
+}
+
+// Regular expression used to match headers for scrubbing
+// The default value is regexp.MustCompile("Authorization")
+func (c *Client) SetScrubHeaders(headers *regexp.Regexp) {
+	c.configuration.scrubHeaders = headers
+}
+
+// Regular expression to match keys in the item payload for scrubbing
+// The default vlaue is regexp.MustCompile("password|secret|token"),
+func (c *Client) SetScrubFields(fields *regexp.Regexp) {
+	c.configuration.scrubFields = fields
+}
+
+// -- Getters
+
+// Rollbar access token.
+func (c *Client) Token() string {
+	return c.configuration.token
+}
+
+// All errors and messages will be submitted under this environment.
+func (c *Client) Environment() string {
+	return c.configuration.environment
+}
+
+// The endpoint used for posting items
+func (c *Client) Endpoint() string {
+	return c.configuration.endpoint
+}
+
+// Platform is the platform reported for all Rollbar items. The default is
+// the running operating system (darwin, freebsd, linux, etc.) but it can
+// also be application specific (client, heroku, etc.).
+func (c *Client) Platform() string {
+	return c.configuration.platform
+}
+
+// String describing the running code version on the server
+func (c *Client) CodeVersion() string {
+	return c.configuration.codeVersion
+}
+
+// host: The server hostname. Will be indexed.
+func (c *Client) ServerHost() string {
+	return c.configuration.serverHost
+}
+
+// root: Path to the application code root, not including the final slash.
+// Used to collapse non-project code when displaying tracebacks.
+func (c *Client) ServerRoot() string {
+	return c.configuration.serverRoot
+}
+
+// custom: Any arbitrary metadata you want to send.
+func (c *Client) Custom() map[string]interface{} {
+	return c.configuration.custom
+}
+
+// Whether or not to use custom client-side fingerprint
+func (c *Client) Fingerprint() bool {
+	return c.configuration.fingerprint
+}
+
+// Regular expression used to match headers for scrubbing
+func (c *Client) ScrubHeaders() *regexp.Regexp {
+	return c.configuration.scrubHeaders
+}
+
+// Regular expression to match keys in the item payload for scrubbing
+func (c *Client) ScrubFields() *regexp.Regexp {
+	return c.configuration.scrubFields
+}
+
+// -- Error reporting
+
+var noExtras map[string]interface{}
+
+// Error sends an error to Rollbar with the given severity level.
+func (c *Client) Error(level string, err error) {
+	c.ErrorWithExtras(level, err, noExtras)
+}
+
+// Errorf sends an error to Rollbar with the given format string and arguments.
+func (c *Client) Errorf(level string, format string, args ...interface{}) {
+	c.ErrorWithStackSkipWithExtras(level, fmt.Errorf(format, args...), 1, noExtras)
+}
+
+// ErrorWithExtras sends an error to Rollbar with the given severity
+// level with extra custom data.
+func (c *Client) ErrorWithExtras(level string, err error, extras map[string]interface{}) {
+	c.ErrorWithStackSkipWithExtras(level, err, 1, extras)
+}
+
+// RequestError sends an error to Rollbar with the given severity level
+// and request-specific information.
+func (c *Client) RequestError(level string, r *http.Request, err error) {
+	c.RequestErrorWithExtras(level, r, err, noExtras)
+}
+
+// RequestErrorWithExtras sends an error to Rollbar with the given
+// severity level and request-specific information with extra custom data.
+func (c *Client) RequestErrorWithExtras(level string, r *http.Request, err error, extras map[string]interface{}) {
+	c.RequestErrorWithStackSkipWithExtras(level, r, err, 1, extras)
+}
+
+// ErrorWithStackSkip sends an error to Rollbar with the given severity
+// level and a given number of stack trace frames skipped.
+func (c *Client) ErrorWithStackSkip(level string, err error, skip int) {
+	c.ErrorWithStackSkipWithExtras(level, err, skip, noExtras)
+}
+
+// ErrorWithStackSkipWithExtras sends an error to Rollbar with the given
+// severity level and a given number of stack trace frames skipped with
+// extra custom data.
+func (c *Client) ErrorWithStackSkipWithExtras(level string, err error, skip int, extras map[string]interface{}) {
+	body := c.buildBody(level, err.Error(), extras)
+	addErrorToBody(c.configuration, body, err, skip)
+	c.push(body)
+}
+
+// RequestErrorWithStackSkip sends an error to Rollbar with the given
+// severity level and a given number of stack trace frames skipped, in
+// addition to extra request-specific information.
+func (c *Client) RequestErrorWithStackSkip(level string, r *http.Request, err error, skip int) {
+	c.RequestErrorWithStackSkipWithExtras(level, r, err, skip, noExtras)
+}
+
+// RequestErrorWithStackSkipWithExtras sends an error to Rollbar with
+// the given severity level and a given number of stack trace frames
+// skipped, in addition to extra request-specific information and extra
+// custom data.
+func (c *Client) RequestErrorWithStackSkipWithExtras(level string, r *http.Request, err error, skip int, extras map[string]interface{}) {
+	body := c.buildBody(level, err.Error(), extras)
+	data := addErrorToBody(c.configuration, body, err, skip)
+	data["request"] = c.requestDetails(r)
+	c.push(body)
+}
+
+// -- Message reporting
+
+// Message sends a message to Rollbar with the given severity level.
+func (c *Client) Message(level string, msg string) {
+	c.MessageWithExtras(level, msg, noExtras)
+}
+
+// MessageWithExtras sends a message to Rollbar with the given severity
+// level with extra custom data.
+func (c *Client) MessageWithExtras(level string, msg string, extras map[string]interface{}) {
+	body := c.buildBody(level, msg, extras)
+	data := body["data"].(map[string]interface{})
+	data["body"] = messageBody(msg)
+	c.push(body)
+}
+
+// RequestMessage sends a message to Rollbar with the given severity level
+// and request-specific information.
+func (c *Client) RequestMessage(level string, r *http.Request, msg string) {
+	c.RequestMessageWithExtras(level, r, msg, noExtras)
+}
+
+// RequestMessageWithExtras sends a message to Rollbar with the given
+// severity level and request-specific information with extra custom data.
+func (c *Client) RequestMessageWithExtras(level string, r *http.Request, msg string, extras map[string]interface{}) {
+	body := c.buildBody(level, msg, extras)
+	data := body["data"].(map[string]interface{})
+	data["body"] = messageBody(msg)
+	data["request"] = c.requestDetails(r)
+	c.push(body)
+}
+
+// -- Misc.
+
+// Wait will call the Wait method of the Transport. If using an asyncronous
+// transport then this will blow until until the queue of
+// errors / messages is empty. If using a syncronous transport then there
+// is no queue so this will be a no-op.
+func (c *Client) Wait() {
+	c.Transport.Wait()
+}
+
+// Close delegates to the Close method of the Transport. For the asyncronous
+// transport this is an alias for Wait, and is a no-op for the synchronous
+// transport.
+func (c *Client) Close() error {
+	return c.Transport.Close()
+}
+
+// Build the main JSON structure that will be sent to Rollbar with the
+// appropriate metadata.
+func (c *Client) buildBody(level, title string, extras map[string]interface{}) map[string]interface{} {
+	return buildBody(c.configuration, level, title, extras)
+}
+
+// Extract error details from a Request to a format that Rollbar accepts.
+func (c *Client) requestDetails(r *http.Request) map[string]interface{} {
+	return requestDetails(c.configuration, r)
+}
+
+// -- POST handling
+
+// Queue the given JSON body to be POSTed to Rollbar.
+func (c *Client) push(body map[string]interface{}) error {
+	return c.Transport.Send(body)
+}
+
+// -- Internal
+
 type configuration struct {
-	token         string
-	environment   string
-	platform      string
-	codeVersion   string
-	serverHost    string
-	serverRoot    string
-	endpoint      string
-	custom        map[string]interface{}
-	fingerprint   bool
-	filterHeaders *regexp.Regexp
-	filterFields  *regexp.Regexp
+	token        string
+	environment  string
+	platform     string
+	codeVersion  string
+	serverHost   string
+	serverRoot   string
+	endpoint     string
+	custom       map[string]interface{}
+	fingerprint  bool
+	scrubHeaders *regexp.Regexp
+	scrubFields  *regexp.Regexp
 }
 
 func createConfiguration(token, environment, codeVersion, serverHost, serverRoot string) configuration {
@@ -118,106 +317,17 @@ func createConfiguration(token, environment, codeVersion, serverHost, serverRoot
 		hostname, _ = os.Hostname()
 	}
 	return configuration{
-		token:         token,
-		environment:   environment,
-		platform:      runtime.GOOS,
-		endpoint:      "https://api.rollbar.com/api/1/item/",
-		filterHeaders: regexp.MustCompile("Authorization"),
-		filterFields:  regexp.MustCompile("password|secret|token"),
-		codeVersion:   codeVersion,
-		serverHost:    hostname,
-		serverRoot:    serverRoot,
-		fingerprint:   false,
+		token:        token,
+		environment:  environment,
+		platform:     runtime.GOOS,
+		endpoint:     "https://api.rollbar.com/api/1/item/",
+		scrubHeaders: regexp.MustCompile("Authorization"),
+		scrubFields:  regexp.MustCompile("password|secret|token"),
+		codeVersion:  codeVersion,
+		serverHost:   hostname,
+		serverRoot:   serverRoot,
+		fingerprint:  false,
 	}
-}
-
-// Build the main JSON structure that will be sent to Rollbar with the
-// appropriate metadata.
-func buildBody(configuration configuration, level, title string, extras map[string]interface{}) map[string]interface{} {
-	timestamp := time.Now().Unix()
-
-	custom := configuration.custom
-	for k, v := range extras {
-		custom[k] = v
-	}
-
-	data := map[string]interface{}{
-		"environment":  configuration.environment,
-		"title":        title,
-		"level":        level,
-		"timestamp":    timestamp,
-		"platform":     configuration.platform,
-		"language":     "go",
-		"code_version": configuration.codeVersion,
-		"server": map[string]interface{}{
-			"host": configuration.serverHost,
-			"root": configuration.serverRoot,
-		},
-		"notifier": map[string]interface{}{
-			"name":    NAME,
-			"version": VERSION,
-		},
-		"custom": custom,
-	}
-
-	return map[string]interface{}{
-		"access_token": configuration.token,
-		"data":         data,
-	}
-}
-
-func addErrorToBody(configuration configuration, body map[string]interface{}, err error, skip int) map[string]interface{} {
-	data := body["data"].(map[string]interface{})
-	errBody, fingerprint := errorBody(configuration, err, skip)
-	data["body"] = errBody
-	if configuration.fingerprint {
-		data["fingerprint"] = fingerprint
-	}
-	return data
-}
-
-func requestDetails(configuration configuration, r *http.Request) map[string]interface{} {
-	cleanQuery := filterParams(configuration.filterFields, r.URL.Query())
-
-	return map[string]interface{}{
-		"url":     r.URL.String(),
-		"method":  r.Method,
-		"headers": flattenValues(filterParams(configuration.filterHeaders, r.Header)),
-
-		// GET params
-		"query_string": url.Values(cleanQuery).Encode(),
-		"GET":          flattenValues(cleanQuery),
-
-		// POST / PUT params
-		"POST":    flattenValues(filterParams(configuration.filterFields, r.Form)),
-		"user_ip": r.RemoteAddr,
-	}
-}
-
-// filterParams filters sensitive information like passwords from being sent to
-// Rollbar.
-func filterParams(pattern *regexp.Regexp, values map[string][]string) map[string][]string {
-	for key := range values {
-		if pattern.Match([]byte(key)) {
-			values[key] = []string{FILTERED}
-		}
-	}
-
-	return values
-}
-
-func flattenValues(values map[string][]string) map[string]interface{} {
-	result := make(map[string]interface{})
-
-	for k, v := range values {
-		if len(v) == 1 {
-			result[k] = v[0]
-		} else {
-			result[k] = v
-		}
-	}
-
-	return result
 }
 
 // -- POST handling
---
+++ transforms.go
@@ -0,0 +1,188 @@
+package rollbar
+
+import (
+	"fmt"
+	"hash/adler32"
+	"net/http"
+	"net/url"
+	"reflect"
+	"regexp"
+	"strings"
+	"time"
+)
+
+// Build the main JSON structure that will be sent to Rollbar with the
+// appropriate metadata.
+func buildBody(configuration configuration, level, title string, extras map[string]interface{}) map[string]interface{} {
+	timestamp := time.Now().Unix()
+
+	custom := configuration.custom
+	for k, v := range extras {
+		custom[k] = v
+	}
+
+	data := map[string]interface{}{
+		"environment":  configuration.environment,
+		"title":        title,
+		"level":        level,
+		"timestamp":    timestamp,
+		"platform":     configuration.platform,
+		"language":     "go",
+		"code_version": configuration.codeVersion,
+		"server": map[string]interface{}{
+			"host": configuration.serverHost,
+			"root": configuration.serverRoot,
+		},
+		"notifier": map[string]interface{}{
+			"name":    NAME,
+			"version": VERSION,
+		},
+		"custom": custom,
+	}
+
+	return map[string]interface{}{
+		"access_token": configuration.token,
+		"data":         data,
+	}
+}
+
+func addErrorToBody(configuration configuration, body map[string]interface{}, err error, skip int) map[string]interface{} {
+	data := body["data"].(map[string]interface{})
+	errBody, fingerprint := errorBody(configuration, err, skip)
+	data["body"] = errBody
+	if configuration.fingerprint {
+		data["fingerprint"] = fingerprint
+	}
+	return data
+}
+
+func requestDetails(configuration configuration, r *http.Request) map[string]interface{} {
+	cleanQuery := filterParams(configuration.scrubFields, r.URL.Query())
+
+	return map[string]interface{}{
+		"url":     r.URL.String(),
+		"method":  r.Method,
+		"headers": flattenValues(filterParams(configuration.scrubHeaders, r.Header)),
+
+		// GET params
+		"query_string": url.Values(cleanQuery).Encode(),
+		"GET":          flattenValues(cleanQuery),
+
+		// POST / PUT params
+		"POST":    flattenValues(filterParams(configuration.scrubFields, r.Form)),
+		"user_ip": r.RemoteAddr,
+	}
+}
+
+// filterParams filters sensitive information like passwords from being sent to
+// Rollbar.
+func filterParams(pattern *regexp.Regexp, values map[string][]string) map[string][]string {
+	for key := range values {
+		if pattern.Match([]byte(key)) {
+			values[key] = []string{FILTERED}
+		}
+	}
+
+	return values
+}
+
+func flattenValues(values map[string][]string) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	for k, v := range values {
+		if len(v) == 1 {
+			result[k] = v[0]
+		} else {
+			result[k] = v
+		}
+	}
+
+	return result
+}
+
+// Build an error inner-body for the given error. If skip is provided, that
+// number of stack trace frames will be skipped. If the error has a Cause
+// method, the causes will be traversed until nil.
+func errorBody(configuration configuration, err error, skip int) (map[string]interface{}, string) {
+	var parent error
+	traceChain := []map[string]interface{}{}
+	fingerprint := ""
+	for {
+		stack := getOrBuildStack(err, parent, skip)
+		traceChain = append(traceChain, buildTrace(err, stack))
+		if configuration.fingerprint {
+			fingerprint = fingerprint + stack.Fingerprint()
+		}
+		parent = err
+		err = getCause(err)
+		if err == nil {
+			break
+		}
+	}
+	errBody := map[string]interface{}{"trace_chain": traceChain}
+	return errBody, fingerprint
+}
+
+// builds one trace element in trace_chain
+func buildTrace(err error, stack Stack) map[string]interface{} {
+	message := nilErrTitle
+	if err != nil {
+		message = err.Error()
+	}
+	return map[string]interface{}{
+		"frames": stack,
+		"exception": map[string]interface{}{
+			"class":   errorClass(err),
+			"message": message,
+		},
+	}
+}
+
+func getCause(err error) error {
+	if cs, ok := err.(CauseStacker); ok {
+		return cs.Cause()
+	} else {
+		return nil
+	}
+}
+
+// gets Stack from errors that provide one of their own
+// otherwise, builds a new stack
+func getOrBuildStack(err error, parent error, skip int) Stack {
+	if cs, ok := err.(CauseStacker); ok {
+		if s := cs.Stack(); s != nil {
+			return s
+		}
+	} else {
+		if _, ok := parent.(CauseStacker); !ok {
+			return BuildStack(4 + skip)
+		}
+	}
+
+	return make(Stack, 0)
+}
+
+// Build a message inner-body for the given message string.
+func messageBody(s string) map[string]interface{} {
+	return map[string]interface{}{
+		"message": map[string]interface{}{
+			"body": s,
+		},
+	}
+}
+
+func errorClass(err error) string {
+	if err == nil {
+		return nilErrTitle
+	}
+
+	class := reflect.TypeOf(err).String()
+	if class == "" {
+		return "panic"
+	} else if class == "*errors.errorString" {
+		checksum := adler32.Checksum([]byte(err.Error()))
+		return fmt.Sprintf("{%x}", checksum)
+	} else {
+		return strings.TrimPrefix(class, "*")
+	}
+}
