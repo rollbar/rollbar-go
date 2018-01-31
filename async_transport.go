95ecc4f0217a05bbacc63e70a0a7b5e06d2b77f2
Andrew Weiss <wvvwwvw@gmail.com>
2018-01-31 10:01:10 -0800

    Document AsyncTransport to fix lint

--- async_transport.go
+++ async_transport.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 )
 
+// AsyncTransport is a concrete implementation of the Transport type which communicates with the
+// Rollbar API asynchronously using a buffered channel.
 type AsyncTransport struct {
 	// Rollbar access token used by this transport for communication with the Rollbar API.
 	Token string
@@ -20,6 +22,9 @@ type AsyncTransport struct {
 	waitGroup   sync.WaitGroup
 }
 
+// NewAsyncTransport builds an asynchronous transport which sends data to the Rollbar API at the
+// specified endpoint using the given access token. The channel is limited to the size of the input
+// buffer argument.
 func NewAsyncTransport(token string, endpoint string, buffer int) *AsyncTransport {
 	transport := &AsyncTransport{
 		Token:       token,
@@ -37,6 +42,8 @@ func NewAsyncTransport(token string, endpoint string, buffer int) *AsyncTranspor
 	return transport
 }
 
+// Send the body to Rollbar if the channel is not currently full.
+// Returns ErrBufferFull if the underlying channel is full.
 func (t *AsyncTransport) Send(body map[string]interface{}) error {
 	if len(t.bodyChannel) < t.Buffer {
 		t.waitGroup.Add(1)
@@ -49,23 +56,37 @@ func (t *AsyncTransport) Send(body map[string]interface{}) error {
 	return nil
 }
 
+// Wait blocks until all of the items currently in the queue have been sent.
 func (t *AsyncTransport) Wait() {
 	t.waitGroup.Wait()
 }
 
+// Close is an alias for Wait for the asyncronous transport
 func (t *AsyncTransport) Close() error {
 	t.Wait()
 	return nil
 }
 
+// SetToken updates the token to use for future API requests.
+// Any request that is currently in the queue will use this
+// updated token value. If you want to change the token without
+// affecting the items currently in the queue, use Wait first
+// to flush the queue.
 func (t *AsyncTransport) SetToken(token string) {
 	t.Token = token
 }
 
+// SetEndpoint updates the API endpoint to send items to.
+// Any request that is currently in the queue will use this
+// updated endpoint value. If you want to change the endpoint without
+// affecting the items currently in the queue, use Wait first
+// to flush the queue.
 func (t *AsyncTransport) SetEndpoint(endpoint string) {
 	t.Endpoint = endpoint
 }
 
+// SetLogger updates the logger that this transport uses for reporting errors that occur while
+// processing items.
 func (t *AsyncTransport) SetLogger(logger ClientLogger) {
 	t.Logger = logger
 }
